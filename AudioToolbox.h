3ce8c6ff987814fce21f89887de9d91923e191a0
Gregory John Casamento <greg.casamento@gmail.com>
2012-11-18 01:02:21 +0000

    Initial commit.  Skeleton code for Audiotoolbox.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/audiotoolbox/trunk@35800 72102866-910b-0410-8b05-ffd578937521

---
+++ AudioToolbox.h
@@ -0,0 +1,21 @@
+/* This file is part of GNUstep */
+
+#ifndef AUDIOTOOLBOX_INCLUDE
+#define AUDIOTOOLBOX_INCLUDE
+
+#include <AudioToolbox/AudioConverter.h>
+#include <AudioToolbox/AudioFile.h>
+#include <AudioToolbox/AudioFileComponent.h>
+#include <AudioToolbox/AudioFileStream.h>
+#include <AudioToolbox/AudioFormat.h>
+#include <AudioToolbox/AudioQueue.h>
+#include <AudioToolbox/AudioUnitUtilities.h>
+#include <AudioToolbox/AUGraph.h>
+#include <AudioToolbox/AUMIDIController.h>
+#include <AudioToolbox/CAFFile.h>
+#include <AudioToolbox/CodeAudioClock.h>
+#include <AudioToolbox/ExtendedAudioFile.h>
+#include <AudioToolbox/MusicPlayer.h>
+#include <AudioToolbox/AudioServices.h>
+
+#endif
